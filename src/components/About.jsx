2157cb6dc524b02fbe3bde08829e2f796e3e4c3f
Subarna Gurung <sg@praktikcenter.lan>
2022-11-23 14:49:44 +0100

    Almost completed

---
+++ src/components/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800
+    to-black text-white'>
+        <div className='max-w-screen-lg p-4 mx-auto flex
+        flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
+                    About
+                </p>
+            </div>
+            <p className='text-xl mt-20'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est non quis sit, beatae aliquid at. Explicabo, aperiam ab necessitatibus tempora commodi culpa accusantium sunt voluptatem assumenda fugiat possimus recusandae et velit, eius amet, aut unde? Aliquam.</p>
+            <br/>
+
+            <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita quas perspiciatis consectetur. Ducimus sequi vero officiis quo, natus porro optio ipsum inventore. Illo, iste asperiores omnis odit exercitationem accusamus! Deserunt quam porro quis quidem maiores corrupti alias aliquam minus! Voluptates, praesentium. Itaque soluta vel repellendus, assumenda debitis dicta natus accusantium sunt rerum corporis libero omnis quisquam, repellat molestias placeat minima autem in!</p>
+        </div>
+    </div>
+  )
+}
+
+export default About
